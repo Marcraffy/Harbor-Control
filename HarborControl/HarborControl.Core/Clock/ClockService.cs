945c20d2063e3e9b26abd625f4ac5f83a8c7f990
Marc Raftesath <marcraffy@gmail.com>
2021-06-20 14:20:38 +0200

    Add multiplier to clock

--- HarborControl/HarborControl.Core/Clock/ClockService.cs
+++ HarborControl/HarborControl.Core/Clock/ClockService.cs
@@ -5,6 +5,15 @@ namespace HarborControl.Core.Clock
 {
     public class ClockService : BaseService, IClockService
     {
+        private int multiplier = 1;
+
+        public ClockService()
+        {
+
+        }
+
         public DateTime CurrentTime => DateTime.Now;
+
+        public int Multiplier { get => multiplier; set => multiplier = value; }
     }
 }
--- HarborControl/HarborControl.Interfaces/Services/IClockService.cs
+++ HarborControl/HarborControl.Interfaces/Services/IClockService.cs
@@ -5,5 +5,7 @@ namespace HarborControl.Interfaces.Services
     public interface IClockService : IService
     {
         public DateTime CurrentTime { get; }
+
+        public int Multiplier { get; set; }
     }
 }
