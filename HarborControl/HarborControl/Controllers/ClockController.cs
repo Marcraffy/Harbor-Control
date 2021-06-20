6b78c3cc96fda709d7e25eff557afc0cb9485ee4
Marc Raftesath <marcraffy@gmail.com>
2021-06-20 16:01:08 +0200

    Add controllers

---
+++ HarborControl/HarborControl/Controllers/ClockController.cs
@@ -0,0 +1,36 @@
+﻿using HarborControl.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace HarborControl.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClockController : ControllerBase
+    {
+        private readonly ILogger<ClockController> logger;
+        private readonly IClockService clockService;
+
+        public ClockController(ILogger<ClockController> logger,
+            IClockService clockService)
+        {
+            this.logger = logger;
+            this.clockService = clockService;
+        }
+
+        [HttpGet]
+        public DateTime Get()
+        {
+            logger.LogInformation($"Time requested");
+            return clockService.CurrentTime;
+        }
+
+        [HttpPost]
+        public void Post([FromQuery] int multiplier)
+        {
+            clockService.Multiplier = multiplier;
+            logger.LogInformation($"Multiplier set to {multiplier}");
+        }
+    }
+}
---
+++ HarborControl/HarborControl/Controllers/WeatherController.cs
@@ -0,0 +1,28 @@
+﻿using HarborControl.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HarborControl.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly ILogger<WeatherController> logger;
+        private readonly IWeatherService weatherService;
+
+        public WeatherController(ILogger<WeatherController> logger,
+            IWeatherService weatherService)
+        {
+            this.logger = logger;
+            this.weatherService = weatherService;
+        }
+
+        [HttpGet]
+        public float Get()
+        {
+            logger.LogInformation($"Windspeed requested");
+            return weatherService.WindSpeed;
+        }
+    }
+}
